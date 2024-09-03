9fd6ff4b4cd22bc5304f5a5387c558f300f89090
sfgardev <sf.garol@gmail.com>
2024-09-03 18:41:29 +0700

    BD-12 chore: clean up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,ts,jsx,tsx}',
-    // './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    // './components/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   plugins: [],
   theme: {
     extend: {},
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { join, resolve } from 'node:path'
 
 import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
-// import dts from 'vite-plugin-dts'
-
 import tailwindcss from 'tailwindcss'
+import { defineConfig } from 'vite'
 
 import { dependencies, devDependencies } from './package.json'
 
@@ -38,41 +36,5 @@ export default defineConfig({
       plugins: [tailwindcss()],
     },
   },
-  plugins: [
-    react(),
-    // dts({ rollupTypes: true }), // Output .d.ts files
-  ],
+  plugins: [react()],
 })
-
-// import { resolve } from 'path'
-
-// import react from '@vitejs/plugin-react-swc'
-// import { defineConfig } from 'vite'
-// import dts from 'vite-plugin-dts'
-
-// import { dependencies, devDependencies } from './package.json'
-// export default defineConfig({
-//   build: {
-//     lib: {
-//       entry: resolve(__dirname, 'src/index.ts'),
-//       fileName: 'index',
-//       name: 'uikit-inctagram',
-//     },
-//     rollupOptions: {
-//       external: [
-//         ...Object.keys(dependencies),
-//         ...Object.keys(devDependencies),
-//         'react/jsx-runtime',
-//       ],
-//       output: {
-//         dir: 'dist',
-//         entryFileNames: '[name].js',
-//         format: 'es',
-//         globals: {
-//           react: 'React',
-//         },
-//       },
-//     },
-//   },
-//   plugins: [react(), dts({ rollupTypes: true })],
-// })
