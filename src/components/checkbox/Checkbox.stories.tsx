bca52625f2c76d51c4cbd3cb9738a72155878eba
Kate <kate-ternovskaya99@mail.ru>
2024-09-09 12:47:36 +0300

    BD-9 fix: replacing title in stories from example to components

--- src/components/checkbox/Checkbox.stories.tsx
+++ src/components/checkbox/Checkbox.stories.tsx
@@ -13,7 +13,7 @@ const meta = {
     layout: 'centered',
   },
   tags: ['autodocs'],
-  title: 'Example/Checkbox',
+  title: 'Components/Checkbox',
 } satisfies Meta<typeof Checkbox>
 
 export default meta
