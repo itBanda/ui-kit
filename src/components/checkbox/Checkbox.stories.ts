54ee92e01658f907e5403057133feafbb8ac5474
Kate <kate-ternovskaya99@mail.ru>
2024-09-09 15:25:18 +0300

    BD-9 fix: refactor checkbox component

--- src/components/checkbox/Checkbox.stories.ts
+++ src/components/checkbox/Checkbox.stories.ts
@@ -3,41 +3,27 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { Checkbox } from './Checkbox'
 
 const meta = {
-  argTypes: {
-    checked: { control: 'boolean' },
-    disabled: { control: 'boolean' },
-    label: { control: 'text' },
-  },
   component: Checkbox,
   parameters: {
     layout: 'centered',
   },
-  tags: ['autodocs'],
   title: 'Components/Checkbox',
 } satisfies Meta<typeof Checkbox>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
-const CheckboxTemplate = (args: any) => {
-  return <Checkbox {...args} />
-}
-
 export const Default: Story = {
   args: {
     checked: true,
-    id: '123',
     label: 'Check-box',
   },
-  render: CheckboxTemplate,
 }
 
 export const Disabled: Story = {
   args: {
     checked: true,
     disabled: true,
-    id: '123',
     label: 'Check-box',
   },
-  render: CheckboxTemplate,
 }
--- src/components/checkbox/Checkbox.tsx
+++ src/components/checkbox/Checkbox.tsx
@@ -1,63 +1,76 @@
-import { ComponentProps, useState } from 'react'
+import { ComponentProps, forwardRef, useId } from 'react'
 
+import { cn } from '../../../src/utils'
 import { Icon } from '../icon/Icon'
 
 type CheckboxProps = {
-  id: string
+  id?: string
   label?: string
 } & ComponentProps<'input'>
 
-export const Checkbox = ({ checked, disabled, id, label }: CheckboxProps) => {
-  const [isChecked, setIsChecked] = useState(checked)
+export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
+  ({ checked, id, label, ...props }, ref) => {
+    const uniqueId = useId()
+    const checkboxId = id ?? uniqueId
 
-  const handleCheckboxChange = () => {
-    if (!disabled) {
-      setIsChecked(!isChecked)
-    }
-  }
-
-  const cursorView = disabled
-    ? 'cursor-not-allowed opacity-50'
-    : 'cursor-pointer'
-
-  const pseudoClasses = disabled
-    ? ''
-    : 'group-focus-within:bg-dark-500 group-hover:bg-dark-300 group-active:bg-dark-100'
-
-  const iconColor = disabled
-    ? 'cursor-not-allowed text-light-100'
-    : 'text-dark-900'
-
-  const checkboxBg = isChecked ? 'border-white bg-white' : ''
-  const disabledCheckboxBg = disabled ? 'border-none bg-dark-100' : ''
-
-  return (
-    <label
-      className={`${cursorView} group relative flex items-center gap-0.5`}
-      id={id}
-    >
-      <div
-        className={`relative h-10 w-10 rounded-full transition-colors duration-200 ${pseudoClasses}`}
+    return (
+      <label
+        className={cn(
+          'group relative flex cursor-pointer items-center gap-0.5 disabled:cursor-not-allowed disabled:opacity-50'
+        )}
+        htmlFor={checkboxId}
       >
-        <input
-          checked={isChecked}
-          className='absolute h-full w-full cursor-pointer opacity-0'
-          disabled={disabled}
-          onChange={handleCheckboxChange}
-          type='checkbox'
-        />
-        <span
-          className={`absolute left-1/2 top-1/2 h-4 w-4 -translate-x-1/2 -translate-y-1/2 transform rounded-sm border-2 border-[#EDF3FA] bg-black transition-all duration-200 ${checkboxBg} ${disabledCheckboxBg}`}
-        >
-          {isChecked && (
-            <Icon
-              className={`${iconColor} absolute left-1/2 top-1/2 h-5 w-5 -translate-x-1/2 -translate-y-1/2 transform`}
-              icon='checkmark-outline'
-            />
+        <div
+          className={cn(
+            'relative h-10 w-10 rounded-full transition-colors duration-200',
+            {
+              'group-focus-within:bg-dark-500 group-hover:bg-dark-300 group-active:bg-dark-100':
+                !props.disabled,
+            }
           )}
-        </span>
-      </div>
-      {label && <span className={`text-white ${cursorView}`}>{label}</span>}
-    </label>
-  )
-}
+        >
+          <input
+            checked={checked}
+            className={cn(
+              'absolute h-full w-full cursor-pointer opacity-0 disabled:cursor-not-allowed'
+            )}
+            disabled={props.disabled}
+            id={checkboxId}
+            onChange={props.onChange}
+            ref={ref}
+            type='checkbox'
+            {...props}
+          />
+          <span
+            className={cn(
+              'absolute left-1/2 top-1/2 h-4 w-4 -translate-x-1/2 -translate-y-1/2 transform rounded-sm border-2 border-light-500 bg-black transition-all duration-200 disabled:bg-dark-100',
+              {
+                'bg-light-100': checked,
+              },
+              { 'border-none bg-dark-100': props.disabled }
+            )}
+          >
+            {checked && (
+              <Icon
+                className={cn(
+                  'absolute left-1/2 top-1/2 h-5 w-5 -translate-x-1/2 -translate-y-1/2 transform text-dark-900',
+                  { 'cursor-not-allowed text-light-700': props.disabled }
+                )}
+                icon='checkmark-outline'
+              />
+            )}
+          </span>
+        </div>
+        {label && (
+          <span
+            className={cn('text-sm font-normal text-white', {
+              'cursor-not-allowed text-light-900': props.disabled,
+            })}
+          >
+            {label}
+          </span>
+        )}
+      </label>
+    )
+  }
+)
