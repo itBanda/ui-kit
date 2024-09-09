396aef38e75c9c8a86943074c7b76c308057df40
SlavaMryasov <slavadevbox1@gmail.com>
2024-09-09 10:29:21 +0300

    BD-11 fix: remove additional logic

--- src/components/card/Card.stories.tsx
+++ src/components/card/Card.stories.tsx
@@ -11,15 +11,6 @@ export default meta
 
 export const Default: StoryObj<typeof meta> = {
   args: {
-    backgroundColor: 'bg-[#171717]',
-    border: 'border border-solid border-[#333333]',
-    borderRadius: 'rounded-sm',
-    //   boxShadow,
-    // children: 'stroka',
-    height: 200,
-    margin: 'm-6',
-    padding: 'p-4',
-    textColor: 'text-light-100',
-    width: 200,
+    children: 'dwdwdw',
   },
 }
--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,54 +1,17 @@
-import { ComponentProps, ReactNode } from 'react'
+import { PropsWithChildren } from 'react'
 
 import { cn } from '../../utils'
 
-export type Props = {
-  backgroundColor?: string
-  border?: string
-  borderRadius?: string
-  boxShadow?: string
-  children?: ReactNode | string
-  height?: number | string
-  margin?: number | string
-  padding?: number | string
-  textColor?: string
-  width?: number | string
-} & ComponentProps<'div'>
-
 export const Card = ({
-  backgroundColor = 'bg-[#171717]',
-  border = 'border border-solid border-[#333333]',
-  borderRadius = 'rounded-sm',
-  boxShadow,
   children,
-  height = 100,
-  margin,
-  padding = 1,
-  textColor = 'text-light-100',
-  width = 100,
-  ...props
-}: Props) => {
+  className,
+}: PropsWithChildren<{ className?: string }>) => {
   return (
     <div
       className={cn(
-        backgroundColor,
-        border,
-        borderRadius,
-        boxShadow,
-        typeof height === 'string' && height,
-        typeof width === 'string' && width,
-        typeof padding === 'string' && padding,
-        typeof margin === 'string' && margin,
-        textColor,
-        'transition-all'
+        'w-fit rounded-sm border border-dark-300 bg-dark-500 p-6',
+        className
       )}
-      style={{
-        height: typeof height === 'number' ? `${height}px` : undefined,
-        margin: typeof margin === 'number' ? `${margin}px` : undefined,
-        padding: typeof padding === 'number' ? `${padding}px` : undefined,
-        width: typeof width === 'number' ? `${width}px` : undefined,
-      }}
-      {...props}
     >
       {children}
     </div>
