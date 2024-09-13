0113868a9ab48e0884d185faa50eb9bfae2e73f8
SlavaMryasov <slavadevbox1@gmail.com>
2024-09-13 12:35:01 +0300

    BD-15 fix: add classname to header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,14 +1,22 @@
 import { ReactNode, forwardRef } from 'react'
 
+import { cn } from '../../utils'
+
 type Props = {
   children: ReactNode
+  className?: string
   logo: ReactNode
 }
 
-export const Header = forwardRef<HTMLDivElement, Props>(
-  ({ children, logo }, ref) => {
+export const Header = forwardRef<HTMLElement, Props>(
+  ({ children, className, logo }, ref) => {
     return (
-      <header className='border-b border-dark-300 bg-dark-700 p-0' ref={ref}>
+      <header
+        className={cn('border-b border-dark-300 bg-dark-700 p-0', {
+          className,
+        })}
+        ref={ref}
+      >
         <div className='container mx-auto flex max-w-screen-xl place-content-between justify-between bg-transparent px-16 py-3'>
           {logo}
           {children}
