f47e35b026c5a13dae009fc767570ca00b07cf09
pRomKol <139136169+pRomKol@users.noreply.github.com>
2024-09-23 18:56:29 +0300

    BD-21 feat: add start version of modal component (#27)
    * BD-21 feat: add start version of modal component
    
    * BD-21 fix: add cursor pointer on icon fix styles
    
    * BD-21 fix: add index
    
    * BD-21 fix: add index
    
    * BD-21 feat: add background remove state and add stope propogitation
    
    * BD-21 fix: add display name foward ref and other smal fixes
    
    * BD-21 fix: add display name foward ref and other smal fixes
    
    * BD-21 fix: modal story fix

---
+++ src/components/modal/Modal.tsx
@@ -0,0 +1,75 @@
+import { ComponentProps, PropsWithChildren, forwardRef, useEffect } from 'react'
+
+import { cn } from '../../utils'
+import { Icon } from '../icon'
+
+export type ModalProps = {
+  isOpened: boolean
+  onClose: () => void
+  title: string
+} & ComponentProps<'div'>
+
+export const Modal = forwardRef<HTMLDivElement, PropsWithChildren<ModalProps>>(
+  ({ children, className, isOpened, onClose, title }, ref) => {
+    useEffect(() => {
+      const handleEscape = (event: KeyboardEvent) => {
+        if (event.key === 'Escape') {
+          onClose()
+        }
+      }
+
+      window.addEventListener('keydown', handleEscape)
+
+      return () => {
+        window.removeEventListener('keydown', handleEscape)
+      }
+    }, [onClose])
+
+    if (!isOpened) {
+      return null
+    }
+
+    return (
+      <div
+        className='fixed inset-0 z-50 flex items-center justify-center bg-light-900 bg-opacity-60'
+        onClick={onClose}
+      >
+        <div
+          aria-labelledby='modal-title'
+          className={cn(
+            'w-full max-w-[500px] divide-y-[1px] divide-dark-100 rounded-sm border border-dark-100 bg-dark-300'
+          )}
+          onClick={e => e.stopPropagation()}
+          ref={ref}
+          role='dialog'
+          tabIndex={-1}
+        >
+          <div className='flex flex-row justify-between px-[24px] pb-[11px] pt-[12px]'>
+            <h3
+              className='text-xl font-bold leading-[36px] text-light-100'
+              id='modal-title'
+            >
+              {title}
+            </h3>
+            <Icon
+              className='mt-[7px]'
+              color='white'
+              cursor='pointer'
+              icon='close-outline'
+              onClick={onClose}
+            />
+          </div>
+          <div
+            className={cn(
+              'flex flex-col px-[24px] py-[30px] text-white',
+              className
+            )}
+          >
+            {children}
+          </div>
+        </div>
+      </div>
+    )
+  }
+)
+Modal.displayName = 'Modal'
