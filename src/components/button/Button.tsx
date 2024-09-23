f25a3c6ff20db784b98d50d3c57e7b43529cfed2
Roman <sselbstmord@gmail.com>
2024-09-23 16:02:16 +0300

    BD-31 fix: fix styles problem

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -39,24 +39,25 @@ const buttonVariants = cva(
     variants: {
       disabled: {
         false: '',
-        true: 'cursor-not-allowed',
+        true: 'pointer-events-none',
       },
       variant: {
         outlined: [
-          'bg-transparent text-accent-500 border border-accent-500',
-          'enabled:hover:border-accent-100 enabled:hover:text-accent-100 enabled:active:text-accent-700 enabled:active:border-accent-700 focus:outline-none focus:ring-1 focus:ring-accent-700 focus:text-accent-700',
+          'bg-transparent text-accent-500 ring-1 ring-accent-500',
+          'hover:ring-accent-100 hover:text-accent-100 active:text-accent-700 active:ring-accent-700 focus:outline-none ' +
+            'focus:ring-1 focus:ring-accent-700 focus:text-accent-700',
         ],
         primary: [
           'bg-accent-500',
-          'enabled:hover:bg-accent-100 enabled:active:bg-accent-700 enabled:active:text-light-500 focus:outline-none focus:ring-2 focus:ring-accent-700',
+          'hover:bg-accent-100 active:bg-accent-700 active:text-light-500 focus:outline-none focus:ring-2 focus:ring-accent-700',
         ],
         secondary: [
           'bg-dark-300',
-          'enabled:hover:bg-dark-100 enabled:active:bg-dark-500 enabled:focus:outline-accent-500 focus:outline-none focus:ring-2 focus:ring-accent-300',
+          'hover:bg-dark-100 active:bg-dark-500 focus:outline-none focus:ring-2 focus:ring-accent-300',
         ],
         text: [
           'text-accent-500',
-          'enabled:hover:text-accent-100 enabled:active:text-accent-700 focus:outline-none focus:ring-2 focus:ring-accent-700 ',
+          'hover:text-accent-100 active:text-accent-700 focus:outline-none focus:ring-2 focus:ring-accent-700',
         ],
       },
     },
