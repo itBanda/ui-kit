f1a8477ff81c97aae2e40e5c597449e44efd049e
sfgardev <sf.garol@gmail.com>
2024-09-28 14:12:08 +0700

    BD-37 chore: add .env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,10 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_RECAPTCHA_SITE_KEY: string
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv
+  }
