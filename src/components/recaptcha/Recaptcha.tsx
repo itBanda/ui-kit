80ac815fc68fc7a96a54ab94016df3559c407a7d
sfgardev <sf.garol@gmail.com>
2024-10-01 13:15:32 +0700

    BD-39 fix: remove recaptcha from ui-kut

--- src/components/recaptcha/Recaptcha.tsx
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line import/no-named-as-default
-import ReCAPTCHA from 'react-google-recaptcha'
-
-export const Recaptcha = (props: ReCAPTCHA['props']) => {
-  return <ReCAPTCHA {...props} />
-}
--- src/components/recaptcha/index.ts
+++
@@ -1,1 +0,0 @@
-export { Recaptcha } from './Recaptcha'
