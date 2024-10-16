1ddc16b59af8c0cc5333ce817675cdb9fd8fca5a
anastasia <legkova.anastasia127@gmail.com>
2024-10-16 12:53:24 +0300

    BD-50 fix: styles fix

--- src/components/tabs/Tabs.tsx
+++ src/components/tabs/Tabs.tsx
@@ -16,7 +16,7 @@ export const Tabs = forwardRef<HTMLDivElement, TabsProps>(
   ({ tabsData }, ref) => {
     return (
       <RadixTabs.Root
-        className='flex h-9 w-[972px] flex-col pt-6'
+        className='flex h-9 flex-col pt-6'
         defaultValue={tabsData[0]?.value}
         ref={ref}
       >
@@ -24,18 +24,13 @@ export const Tabs = forwardRef<HTMLDivElement, TabsProps>(
           {tabsData.map(tab => (
             <RadixTabs.Trigger
               className={cn(
-                'flex-1 border-b-2 border-dark-100 px-4 py-1.5 text-base font-semibold text-dark-100 transition duration-200',
-                'focus:rounded-sm focus:outline-none focus:ring-2 focus:ring-inset focus:ring-accent-500',
-                tab.disabled
-                  ? cn(
-                      'cursor-not-allowed border-b-2 border-dark-300 text-dark-300',
-                      'data-[state=active]:border-accent-900 data-[state=active]:text-accent-900'
-                    )
-                  : cn(
-                      'hover:border-b-2 hover:border-dark-100 hover:bg-accent-900/25 hover:text-dark-100',
-                      'active:bg-accent-100/25 data-[state=active]:border-b-2 data-[state=active]:border-blue-500',
-                      'data-[state=active]:text-accent-500'
-                    )
+                'flex-1 border-b-2 px-4 py-1.5 text-base font-semibold transition duration-200 focus:rounded-sm focus:outline-none focus:ring-2 focus:ring-inset',
+                {
+                  'border-dark-100 text-dark-100 hover:border-b-2 hover:border-dark-100 hover:bg-accent-900/25 hover:text-dark-100 focus:ring-accent-500 active:bg-accent-100/25 data-[state=active]:border-b-2 data-[state=active]:border-blue-500 data-[state=active]:text-accent-500':
+                    !tab.disabled,
+                  'cursor-not-allowed border-dark-300 text-dark-300 data-[state=active]:border-accent-900 data-[state=active]:text-accent-900':
+                    tab.disabled,
+                }
               )}
               disabled={tab.disabled}
               key={tab.value}
