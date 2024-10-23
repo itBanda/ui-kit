5370fe0b7119b301e105d2ed8345dc0aad68b2b0
SlavaMryasov <slavadevbox1@gmail.com>
2024-10-23 16:20:02 +0300

    Merge branch 'dev' into feat/BD-59-edit-select-component

--- src/components/tabs/Tabs.tsx
+++ src/components/tabs/Tabs.tsx
@@ -16,7 +16,7 @@ export const Tabs = forwardRef<HTMLDivElement, TabsProps>(
   ({ tabsData }, ref) => {
     return (
       <RadixTabs.Root
-        className='flex h-9 flex-col pt-6'
+        className='flex flex-col'
         defaultValue={tabsData[0]?.value}
         ref={ref}
       >
@@ -26,7 +26,7 @@ export const Tabs = forwardRef<HTMLDivElement, TabsProps>(
               className={cn(
                 'flex-1 border-b-2 px-4 py-1.5 text-base font-semibold transition duration-200 focus:rounded-sm focus:outline-none focus:ring-2 focus:ring-inset',
                 {
-                  'border-dark-100 text-dark-100 hover:border-b-2 hover:border-dark-100 hover:bg-accent-900/25 hover:text-dark-100 focus:ring-accent-500 active:bg-accent-100/25 data-[state=active]:border-b-2 data-[state=active]:border-blue-500 data-[state=active]:text-accent-500':
+                  'border-dark-100 text-dark-100 data-[state=active]:border-b-2 data-[state=active]:border-blue-500 data-[state=active]:text-accent-500 hover:border-b-2 hover:border-dark-100 hover:bg-accent-900/25 hover:text-dark-100 focus:ring-accent-500 active:bg-accent-100/25':
                     !tab.disabled,
                   'cursor-not-allowed border-dark-300 text-dark-300 data-[state=active]:border-accent-900 data-[state=active]:text-accent-900':
                     tab.disabled,
---
+++ src/components/text-area/TextArea.tsx
@@ -0,0 +1,45 @@
+import { ComponentProps, forwardRef } from 'react'
+
+import { useGetId } from '@/hooks'
+import { cn } from '@/utils'
+
+type TextAreaProps = {
+  errorText?: string
+  label?: string
+} & ComponentProps<'textarea'>
+export const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
+  ({ disabled, errorText, label, ...props }, ref) => {
+    const id = useGetId(props.id)
+
+    return (
+      <div className='flex flex-col'>
+        {label && (
+          <label
+            className={cn('text-sm text-light-900', {
+              'text-dark-100': disabled,
+            })}
+            htmlFor={id}
+          >
+            {label}
+          </label>
+        )}
+        <textarea
+          className={cn(
+            'custom-scrollbar w-full resize-none rounded-md border border-dark-100 bg-transparent p-2 text-sm text-light-900 outline-none transition-all scrollbar-thin focus:border-accent-700 focus:text-light-100 active:border-light-100 active:text-light-100 disabled:cursor-not-allowed disabled:text-dark-100',
+            {
+              'border-danger-500 text-light-100': errorText,
+            }
+          )}
+          disabled={disabled}
+          id={id}
+          ref={ref}
+          {...props}
+        />
+        {errorText && (
+          <span className={cn('text-sm text-danger-500')}>{errorText}</span>
+        )}
+      </div>
+    )
+  }
+)
+TextArea.displayName = 'TextArea'
